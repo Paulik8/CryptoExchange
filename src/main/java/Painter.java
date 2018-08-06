a0fe71d72c796cfd77e0a6f97723138e48e4bbe0
Paulik8 <ikhotin@mail.ru>
2018-08-06 20:16:14 +0300

    meet the hard graphic challenge

--- src/main/java/Painter.java
+++ src/main/java/Painter.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
@@ -10,6 +11,9 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -40,16 +44,31 @@ public class Painter extends Application {
     ObservableList<XYChart.Data<String, Double>> xyList2 = FXCollections.observableArrayList();
 
     ObservableList<String> myXaxisCategories = FXCollections.observableArrayList();
-    ObservableList<String> listPairs;
+    ObservableList<String> listPairs = FXCollections.observableArrayList();
+
+
 
     int i;
     int a;
     private Task<Date> task;
+    private Task<String> task2;
     private LineChart<String,Number> lineChart;
     private XYChart.Series xySeries1;
     private XYChart.Series xySeries2;
     private CategoryAxis xAxis;
     private int lastObservedSize;
+
+
+    private void updateMaps(String strDate) {
+        for (Map.Entry<String, Double> entry : pairsEXMO.entrySet()) {
+            ObservableList<XYChart.Data<String, Double>> listBuf = FXCollections.observableArrayList();
+            if (historyEXMO.get(entry.getKey()) != null) {
+                listBuf = historyEXMO.get(entry.getKey());
+            }
+            listBuf.add(new XYChart.Data<>(strDate, entry.getValue()));
+            historyEXMO.put(entry.getKey(), listBuf);
+        }
+    }
 
     @Override
     public void start(Stage stage) {
@@ -66,13 +85,16 @@ public class Painter extends Application {
 
         //lastPairsEXMO = pairsEXMO;
         stage.setTitle("Line Chart Sample");
+
+        ComboBox comboBox = new ComboBox();
+
         xAxis = new CategoryAxis();
         xAxis.setLabel("Date");
+
 
         final NumberAxis yAxis = new NumberAxis();
         lineChart = new LineChart<>(xAxis,yAxis);
 
-        lineChart.setTitle("EOSUSD");
         lineChart.setAnimated(false);
 
         task = new Task<Date>() {
@@ -80,14 +102,9 @@ public class Painter extends Application {
             protected Date call() throws Exception {
                 while (true) {
 
-                    for (Map.Entry<String, Double> entry : pairsEXMO.entrySet()) {
-                        if (entry.getValue().equals(lastPairsEXMO.get(entry.getKey()))) {
-                            a++;
-
-                        }
-                    }
-                    System.out.print(a % 29);
                         if (lastPairsEXMO.size() == 0 ||  !(Objects.equals(lastPairsEXMO.get("BTCUSD"), pairsEXMO.get("BTCUSD")))) {
+                            comboBox.getItems().addAll(pairsEXMO.keySet());//
+
                             System.out.println("newinfo " + lastPairsEXMO.get("BTCUSD") + " " + pairsEXMO.get("BTCUSD") + " " + lastPairsEXMO.size());
                             lastPairsEXMO.put("BTCUSD", pairsEXMO.get("BTCUSD"));
                             lastPairsBITFINEX.put("BTCUSD", pairsBITFINEX.get("BTCUSD"));
@@ -120,35 +137,59 @@ public class Painter extends Application {
                 String strDate = dateFormat.format(newDate);
                 myXaxisCategories.add(strDate);
 
-                xyList1.add(new XYChart.Data(strDate, pairsEXMO.get("BTCUSD")));
+                System.out.println("value: " + comboBox.getValue());
+                updateMaps(strDate);//
+                Iterator<XYChart.Data<String, Double>> iterator = xyList1.iterator();//TODO ценнейшая информация
+                while (iterator.hasNext()) {
+                    System.out.println("iter " + iterator.next().getXValue());
+                }//для изменения списка при перекликивании с одной пары на другую(время будет одно и то же, а цена разная)
+                xyList1.add(new XYChart.Data<>(strDate, pairsEXMO.get(comboBox.getValue().toString())));
+                System.out.println("xylist " + xyList1.size());
+                xyList2.add(new XYChart.Data<>(strDate, pairsBITFINEX.get(comboBox.getValue().toString())));
                 if (xyList1.size() - lastObservedSize > 10) {
                     //lastObservedSize = 1;
                     xAxis.getCategories().remove(0);
                     xyList1.remove(0);
+                    xyList2.remove(0);
                 }
-                System.out.println("xylist " + xyList1.size());
-                xyList2.add(new XYChart.Data(strDate, pairsBITFINEX.get("BTCUSD")));
 
+            }
+        });
+
+
+        comboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (i > 0) {
+                xyList1.remove(0, xyList1.size());
+                xyList1.addAll(historyEXMO.get(comboBox.getValue().toString()));
+                System.out.println("change " + xyList1.size() + newValue);
+                lineChart.setTitle(comboBox.getValue().toString());
             }
         });
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.submit(task);
 
-        Scene scene  = new Scene(lineChart,800,600);
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(comboBox, lineChart);
+        comboBox.getSelectionModel().selectFirst();
+        HBox.setMargin(comboBox, new Insets(70, 0,0,5));
+        lineChart.setMinSize(800, 600);
+
+        Scene scene  = new Scene(hBox, 1000,700);
 
         xAxis.setCategories(myXaxisCategories);
         xAxis.setAutoRanging(false);
 
-        xySeries1 = new XYChart.Series(xyList1);
+        xySeries1 = new XYChart.Series<>(xyList1);
         xySeries1.setName("EXMO");
 
-        xySeries2 = new XYChart.Series(xyList2);
+        xySeries2 = new XYChart.Series<>(xyList2);
         xySeries2.setName("BITFINEX");
 
+        yAxis.setForceZeroInRange(false);
         lineChart.getData().addAll(xySeries1, xySeries2);
 
-        //i = 0;
+        i = 1;
 
         stage.setScene(scene);
         stage.show();
